c9d429e12f6bd1fd7e30fa437465cc3bcef69805
aya <aya147khaled@gmail.com>
2023-07-31 01:14:42 +0300

    Add erase and clear button

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,15 +10,18 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.StrokeLineCap;
+import javafx.scene.shape.StrokeLineJoin;
 import javafx.stage.Stage;
 import javafx.scene.layout.HBox;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 //import application.Main.Shape;
 
-public class Main extends Application {
+public class PaintApp extends Application {
 
     private final List<Shape> shapes = new ArrayList<>();
     private Shape currentShape;
@@ -48,7 +51,7 @@ public class Main extends Application {
         
         
 
-       
+        //rectangle button
         Image rectangleImage = new Image(getClass().getResourceAsStream("rectangle2.png"));
         Button rectangleButton = new Button(null, new javafx.scene.image.ImageView(rectangleImage));
         rectangleButton.setOnAction(event -> {
@@ -62,7 +65,7 @@ public class Main extends Application {
             flag=1;
          
         });
-        
+        //circle
         Image circleImage = new Image(getClass().getResourceAsStream("circle2.png"));
         Button circleButton = new Button(null, new javafx.scene.image.ImageView(circleImage));
         circleButton.setOnAction(event -> {
@@ -76,45 +79,84 @@ public class Main extends Application {
             flag=2;
         
         });
+        //erase
+        Image eraseImage = new Image(getClass().getResourceAsStream("erase3.png"));
+        Button eraseButton = new Button(null, new javafx.scene.image.ImageView( eraseImage));
+        eraseButton.setOnAction(event -> {
+            currentShape = new Erase();
+            currentShape.color = Color.WHITE;
+            isDrawing = true;
+            flag = 4;
+        });
+        
+        // clear 
+        Image clearImage = new Image(getClass().getResourceAsStream("clear2.png"));
+        Button clearButton =  new Button(null, new javafx.scene.image.ImageView( clearImage));
+        clearButton.setOnAction(event -> {
+            gc.setFill(Color.WHITE);
+            gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+            shapes.clear();
+            isDrawing = true;
+            flag = 5;
+        });
        
        
 
         // Add the canvas and button to a layout
         BorderPane root = new BorderPane();
         root.setCenter(canvas); 
-        root.setTop(new HBox(rectangleButton, circleButton, fillComboBox, colorPicker));
+        root.setTop(new HBox(rectangleButton, circleButton, fillComboBox, colorPicker,clearButton,eraseButton));
         
         // Handle mouse events to draw the shapes
         canvas.setOnMousePressed(event -> {
             if (isDrawing) {
-                currentShape.startX = event.getX();
-                currentShape.startY = event.getY();
+                if (flag == 4) { // Brush
+                    currentShape = new Erase();
+                    currentShape.color = Color.WHITE;
+                    isDrawing = true;
+                } else {
+                    currentShape.startX = event.getX();
+                    currentShape.startY = event.getY();
+                }
             }
         });
-
+ 
         canvas.setOnMouseDragged(event -> {
             if (isDrawing) {
-                currentShape.endX = event.getX();
-                currentShape.endY = event.getY();
-                redraw(gc);
+                if (flag == 4) { // erase
+                    ((Erase) currentShape).xPoints.add(event.getX());
+                    ((Erase) currentShape).yPoints.add(event.getY());
+                    redraw(gc);
+                } else {
+                    currentShape.endX = event.getX();
+                    currentShape.endY = event.getY();
+                    redraw(gc);
+                }
             }
         });
 
         canvas.setOnMouseReleased(event -> {
             if (isDrawing) {
-                currentShape.endX = event.getX();
-                currentShape.endY = event.getY();
-                shapes.add(currentShape); 
-                if(flag==1)
-                	currentShape = new Rectangle();
-                else if (flag==2)
-                currentShape = new Circle();
-                currentShape.color = colorPicker.getValue();
-                if (fillComboBox.getValue().equals("Solid")) {
-                    currentShape.isFilled = true;
-                    currentShape.fillColor = colorPicker.getValue();
+                if (flag == 4) { //erase
+                    ((Erase) currentShape).xPoints.add(event.getX());
+                    ((Erase) currentShape).yPoints.add(event.getY());
+                    shapes.add(currentShape);
+                    currentShape = new Erase();
+                    currentShape.color = Color.WHITE;
+                } else {
+                    currentShape.endX = event.getX();
+                    currentShape.endY = event.getY();
+                    shapes.add(currentShape);
+                    if (flag == 1)
+                        currentShape = new Rectangle();
+                    else if (flag == 2)
+                        currentShape = new Circle();
+                    currentShape.color = colorPicker.getValue();
+                    if (fillComboBox.getValue().equals("Solid")) {
+                        currentShape.isFilled = true;
+                        currentShape.fillColor = colorPicker.getValue();
+                    }
                 }
-              //  isDrawing = false;
             }
         });
 
@@ -170,7 +212,30 @@ public class Main extends Application {
             gc.strokeOval(startX - radius, startY - radius, radius * 2, radius * 2);
         }
     }
-    
+    //erase class
+    private class Erase extends Shape {
+        List<Double> xPoints = new ArrayList<>();
+        List<Double> yPoints = new ArrayList<>();
+ 
+        @Override
+        void draw(GraphicsContext gc) {
+            gc.setStroke(Color.WHITE);
+            //gc.setLineWidth(5.0);
+            gc.setLineCap(StrokeLineCap.ROUND);
+            gc.setLineJoin(StrokeLineJoin.ROUND);
+ 
+            for (int i = 0; i < xPoints.size() - 1; i++) {
+                double startX = xPoints.get(i);
+                double startY = yPoints.get(i);
+                double endX = xPoints.get(i + 1);
+                double endY = yPoints.get(i + 1);
+ 
+                double radius = 5.0 / 2.0;
+                gc.strokeOval(startX - radius, startY - radius, radius * 2, radius * 2);
+                gc.strokeLine(startX, startY, endX, endY);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
